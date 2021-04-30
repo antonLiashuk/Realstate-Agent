17f180369491bbcdecc71a3fea218aa367d94e01
antonLiashuk <73991970+antonLiashuk@users.noreply.github.com>
2021-04-30 23:36:18 +0200

    Update Test.java

--- src/multiThreads/Test.java
+++ src/multiThreads/Test.java
@@ -5,8 +5,5 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         new Otodom();
-        /*String antosha = "Antonchik";
-        String antonArray = Arrays.toString(antosha.split("chik"));
-        System.out.println(antonArray);*/
     }
 }
